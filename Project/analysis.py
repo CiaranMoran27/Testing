22070a9b73304e8f91cfb9a58fcc06e84e5608e1
unknown <G00398269@gmit.ie>
2021-04-19 17:02:11 +0100

    test

--- Project/analysis.py
+++ Project/analysis.py
@@ -114,7 +114,11 @@ def plot_box_plot():
 
     ax = sns.boxplot(data = iris_df.loc[:, ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
     plt.suptitle('Plot X: Boxplot of all variables',fontsize = 16)
+    
     ax.set_ylabel('length of variable (cm)', fontsize=12) 
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    
     sns.set(style="darkgrid")
     
     plt.savefig('Images/' + 'box_plot' +'.png')
